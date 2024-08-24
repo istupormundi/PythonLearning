f21f0996c1380a6b589e4a1314c46be58a50342a
istupormundi <dasha.prikhodko@gmail.com>
2024-08-24 15:34:03 +0200

    add print log

--- TestData/exel.py
+++ TestData/exel.py
@@ -17,8 +17,7 @@ for i in range(1, sheet.max_row + 1):
     if sheet.cell(row=i, column=1).value == "Testcase2":
         print(i)
         for j in range(2, sheet.max_column + 1):
-            # print("(i,j) = " "(" + str(i) + ")" + "(" + str(j) + ")")
+            print("(i,j) = " "(" + str(i) + ")" + "(" + str(j) + ")")
             dictionary[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
             # print(sheet.cell(row=i, column=j).value)
 print(dictionary)
-
